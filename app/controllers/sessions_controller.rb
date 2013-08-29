cbb8fd96bb53bdcc2e05290542e0edc30763b04b
Tom Kim <tom.y.kim@gmail.com>
2013-08-29 16:36:18 -0700

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# flash.now[:success] = "Welcome #{[@user]}!"
+			sign_in user
+			redirect_to user
+		else
+			flash.now[:error] = "Invalid email/password.  Please try again." # error message!
+			render 'new'
+		end
+	end
+	
+	def destroy
+		sign_out
+		redirect_to root_url
+	end
+end
